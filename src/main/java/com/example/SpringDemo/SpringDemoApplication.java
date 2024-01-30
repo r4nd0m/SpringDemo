9ac6a7aa6f9126e2a013b2d1d97aaa1127ae5d70
Vladimir Kalenyuk <vladimir.kalenyuk@gmail.com>
2024-01-30 22:13:49 +0100

    add logger and set logging level for local dev environment to DEBUG (don't forget to set the environment variable spring.profiles.active=dev in the run configuration)

--- src/main/java/com/example/SpringDemo/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemo/SpringDemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.SpringDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+		// test
+		logger.info("Application started!");
 	}
 
 }
