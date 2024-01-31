a6d370bcd5a2d6d9b7197e119ae1820b71523931
Vladimir Kalenyuk <vladimir.kalenyuk@gmail.com>
2024-01-31 12:50:08 +0100

    initialize app via SpringApplication class set the custom bannerMode property value

--- src/main/java/com/example/SpringDemo/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemo/SpringDemoApplication.java
@@ -2,8 +2,9 @@ package com.example.SpringDemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringDemoApplication {
@@ -11,8 +12,11 @@ public class SpringDemoApplication {
 	private static final Logger logger = LoggerFactory.getLogger(SpringDemoApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
-		// test
+		new SpringApplicationBuilder()
+				.sources(SpringDemoApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+
 		logger.info("Application started!");
 	}
 
