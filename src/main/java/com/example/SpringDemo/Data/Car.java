4480684047c6e52686e1290ae164e6855aeb915e
Vladimir Kalenyuk <vladimir.kalenyuk@gmail.com>
2024-02-06 13:10:58 +0100

    add mariaDB dependencies and connection settings

---
+++ src/main/java/com/example/SpringDemo/Data/Car.java
@@ -0,0 +1,27 @@
+package com.example.SpringDemo.Data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+}
