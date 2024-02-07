a0c4bd512fe71c388f0505362fa63c122eaebc42
Vladimir Kalenyuk <vladimir.kalenyuk@gmail.com>
2024-02-07 12:48:55 +0100

    add constructors and a car name field, experiment with field renaming

--- src/main/java/com/example/SpringDemo/Data/Car.java
+++ src/main/java/com/example/SpringDemo/Data/Car.java
@@ -11,6 +11,17 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String owner;
+    private String name;
+
+    public Car() {
+
+    }
+
+    public Car(String owner, String name) {
+        super();
+        this.owner = owner;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
@@ -23,5 +34,14 @@ public class Car {
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }
