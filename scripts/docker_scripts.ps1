96e709e44812175b6f35eb6b531f75cf968f5461
Vladimir Kalenyuk <vladimir.kalenyuk@adesso.de>
2024-05-07 14:03:22 +0200

    fix docker namespace name to be able to push built images to the dockerhub

--- scripts/docker_scripts.ps1
+++ scripts/docker_scripts.ps1
@@ -1,7 +1,7 @@
 # IMPORTANT! before building the container make sure to execute gradle build to create a JAR file
 
 # build docker container
-docker buildx b -f Dockerfile --build-arg JAR_FILE=build/libs/SpringDemo-0.0.1-SNAPSHOT.jar -t kalenyuk/spring-demo .
+docker buildx b -f Dockerfile --build-arg JAR_FILE=build/libs/SpringDemo-0.0.1-SNAPSHOT.jar -t vkalenyuk/spring-demo .
 
 # create network to connect the SpringBoot container with the MariaDB container
 docker network create local-docker-network
@@ -14,4 +14,4 @@ docker rm springboot-demo
 
 # start the SpringBoot container attached to the docker network
 # and having the port mapping to the host to be accessible via http://localhost:8080
-docker run -p 8080:8080 --network=local-docker-network --name springboot-demo kalenyuk/spring-demo --attach
+docker run -p 8080:8080 --network=local-docker-network --name springboot-demo vkalenyuk/spring-demo --attach
